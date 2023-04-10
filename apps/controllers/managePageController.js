b008de7e7640146b2943df0859af0e2eb39ce621
HoaAndy <cuduchoa2001@gmail.com>
2023-04-10 20:05:12 +0700

    20:00 10/4 commit

--- apps/controllers/managePageController.js
+++ apps/controllers/managePageController.js
@@ -4,17 +4,17 @@ const Category = require('../models/Category');
 const Cart = require('../models/Cart');
 
 class managePageController {
-    async show(req, res, next) {
-        try {
-            let user = null;
-            if (req.session.token) {
-                user = req.session.username;
-            }
-            res.render('manage');
-        } catch (error) {
-            next(error);
-        }
-    }
+    // async show(req, res, next) {
+    //     try {
+    //         let user = null;
+    //         if (req.session.token) {
+    //             user = req.session.username;
+    //         }
+    //         res.render('manage');
+    //     } catch (error) {
+    //         next(error);
+    //     }
+    // }
 };
 
 module.exports = new managePageController();
