cdea6d644dc87d059485d13045ff96259e95fa12
HoaAndy <cuduchoa2001@gmail.com>
2023-04-13 18:57:27 +0700

    13/4 commit

--- apps/routes/information.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const informationPageController = require('../controllers/informationPageController');
-
-router.get('/', informationPageController.index);
-router.post('/', informationPageController.update);
-
-module.exports = router;
